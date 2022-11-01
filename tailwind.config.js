13f60790f71e0c48df1578ed2d564d4b81f88c54
chuhieu03 <chuhieuqb2003@gmail.com>
2022-11-01 14:04:43 +0700

    update

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  content: ["./src/**/*.{html, js, ts, vue}", "./src/**/*"],
+  theme: {
+    extend: {
+      screens: {
+        mobile: "400px",
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        xxl: "1536px",
+        xxxl: "1900px",
+      },
+      colors: {
+        "aqua-rgba": "rgba(6, 243, 243, 0.995)",
+        "black-rgba": "rgba(0, 0, 0, 0.64)",
+      },
+      backgroundImage: {
+        footer: "url('./Vector22.svg')",
+        circle: "url('./Ellipse 1126.svg')",
+        circlew: "url('./Ellipse 1126w.svg')",
+      },
+    },
+    fontFamily: {
+      roboto: ["Roboto"],
+    },
+  },
+  plugins: [],
+};
